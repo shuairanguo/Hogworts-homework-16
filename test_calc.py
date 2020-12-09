c07bd2ee128e31005eb9aeda453a587c49c72ee6
guoshuairan <guoshuairan@foxmail.com>
2020-12-09 21:50:26 +0800

    homework

---
+++ test_calc.py
@@ -0,0 +1,36 @@
+import pytest
+from pythoncode.calculator import Calculator
+
+
+class TestCalc:
+    def setup_class(self):
+        self.calc = Calculator()
+        print("开始计算")
+
+    def teardown_class(self):
+        print("结束计算")
+
+    @pytest.mark.parametrize("a,b,expect", [
+        (3, 5, 8), (-1, -2, -3), (100, 300, 400)
+    ], ids=["int", "minus", "bigint"])
+    def test_add(self, a, b, expect):
+        assert expect == self.calc.add(a, b)
+
+    @pytest.mark.parametrize("a,b,expect", [
+        (10, 1, 9), (0, 0, 0), (-1, -1, 0)
+    ], ids=["int", "minus", "bigint"])
+    def test_sub(self, a, b, expect):
+        assert expect == self.calc.sub(a, b)
+
+    @pytest.mark.parametrize("a,b,expect", [
+        (0, 1000, 0), (100, 100, 10000), (-1, -1, 1)
+    ], ids=["int", "minus", "bigint"])
+    def test_mul(self, a, b, expect):
+        assert expect == self.calc.mul(a, b)
+
+    @pytest.mark.parametrize("a,b,expect", [
+        (10, 4, 2.5), (0, 100, 0), (1000, 1, 1000)
+    ], ids=["int", "minus", "bigint"])
+    def test_mul(self, a, b, expect):
+        assert expect == self.calc.div(a, b)
+
