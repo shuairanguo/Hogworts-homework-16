5208d31c46e44458e0cae186786cc46bfe1d4b31
guoshuairan <guoshuairan@foxmail.com>
2021-01-10 21:27:34 +0800

    测试框架-日志、截图、录屏

--- test_frame/page/search.py
+++ test_frame/page/search.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.common.by import By
 
 from test_frame.base_page import BasePage
+from test_frame.page.pre_page import PrePage
 
 
-class Search(BasePage):
+class Search(PrePage):
     def search(self):
-        self.find(By.XPATH,'//*[@resource-id="com.xueqiu.android:id/search_input_text"]').send_keys()
+        # self.find(By.XPATH,'//*[@resource-id="com.xueqiu.android:id/search_input_text"]').send_keys()
+        self.basepage.load('../page/search.yaml')
         return True
